180cc9300c64fa537322c4e56e8afccbe7927dff
Simon-Sithole <202406543@ub.ac.bw>
2025-04-22 16:50:17 +0200

    Update Undergraduate.java
    added comments

--- Undergraduate.java
+++ Undergraduate.java
@@ -1,10 +1,10 @@
 public class Undergraduate extends Student{
     private int yearOfStudy;
-    
+    // constructors
     public Undergraduate(String name , int studentID , double grade,int yearOfStudy){
     super(name,studentID,grade);
     this.yearOfStudy= yearOfStudy;
-    
+// to override super class
     }
     @Override
     public void displaydetails(){
