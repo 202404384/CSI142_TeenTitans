695f2cb1b9dc54418ae82031a7b0392b5bc464ee
202404384 <202404384@ub.ac.bw>
2025-04-22 15:57:47 +0200

    domain class with setter and getter methods

---
+++ Student.java
@@ -0,0 +1,45 @@
+public class Student {
+
+    private String name;
+    private int studentID;
+    private double grade;
+
+
+    public Student (String name , int studentID , double grade)  {
+        this.name = name;
+        this.studentID = studentID;
+        this.grade = grade;
+    }
+
+
+
+    public String getName() {
+         return name;
+         }
+    public int getStudentID() {
+         return studentID; 
+        }
+    public double getGrade() { 
+        return grade; 
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setStudentID(int studentID){
+        this.studentID=studentID;
+    }
+    public void setGrade(double grade){
+        this.grade=grade;
+    }
+
+
+
+
+
+    @Override
+public String toString() {
+    return "Student name:" + name + "StudentID" + studentID + "Student Grade" + grade ; 
+}
+
+}
+
